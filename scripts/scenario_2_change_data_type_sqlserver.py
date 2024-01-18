260d0fd55a8cfaa0ff5ab311d317d3562b21820d
Levi De Andrade <Levi.DeAndrade@heat.com>
2024-01-18 16:26:52 -0500

    update scritts to drop constrain

--- scripts/scenario_2_change_data_type_sqlserver.py
+++ scripts/scenario_2_change_data_type_sqlserver.py
@@ -30,17 +30,17 @@ def remove_parenteses(texto):
 Base = declarative_base()
 
 
-user = "levi"
-password = "3523"
-server_name = "DESKTOP-O17EGB4"
-main_db = "STANDARD"
+user = "Levi.deAndrade"
+password = "#l3v^!d$ApP%^20*&23_"
+server_name = "azsqlserver-601a-sqlprod.database.windows.net"
+main_db = "APP_MASTER_CHECKER_DEV"
 driver_odbc = "ODBC+Driver+17+for+SQL+Server"
 
 
 main_schema = "dbo"
-cloned_db = "CUSTOMER_1"
+cloned_db = "APP_MASTER_CHECKER_DEV"
 cloned_schema = "dbo"
-table_name = "USER_"
+table_name = "STANDARD"
 
 try:
 # Conecction to main db
@@ -72,6 +72,7 @@ try:
     # Getting table's name
     cloned_tables = inspector_clone.get_table_names(schema=cloned_schema)
     # Cloned Columns type informations
+    main_table_name = "CUSTOMER_1"
     columns_target_type = {c['name']: c['type'] for c in inspector_clone.get_columns(main_table_name, schema=cloned_schema)}
 except Exception as e:
     print(e)
@@ -81,20 +82,25 @@ if main_table_name in cloned_tables:
     print(f"table {main_table_name} found in {cloned_tables}")
 
     for column_target in columns_target_type.keys():
+        drop_constraint = ""
+        print(column_target)
         if column_target in columns_source_type.keys():
             if (remove_parenteses(str(columns_target_type[column_target])) != remove_parenteses(str(columns_source_type[column_target]))) is True:
                 try:
                     query = f"""SELECT name
                                 FROM sys.objects
                                 where SCHEMA_NAME(schema_id) = '{cloned_schema}'
-                                and name like '%{table_name}%'
+                                and name like '%{main_table_name}%'
                                 and name like '%{column_target[:9]}%'
                                 and type_desc like 'DEFAULT%' """
 
-                    drop_constraint = DDL(f"""ALTER TABLE {cloned_schema}.{main_table_name} DROP CONSTRAINT {execute_query_and_store_result(cloned_uri,query)}""")
+                    drop_constraint = DDL(f"""BEGIN TRY
+                                                ALTER TABLE {cloned_schema}.{main_table_name} DROP CONSTRAINT {execute_query_and_store_result(cloned_uri,query)}
+                                              END TRY BEGIN CATCH END CATCH""")
                     print(f" !CONSTRAINT {execute_query_and_store_result(cloned_uri,query)} DROPPED FOR THIS OPERATION")
                     event.listen(Base.metadata, 'before_create', drop_constraint.execute_if(dialect=mssql.dialect()))
                 except Exception as e:
+                    del drop_constraint
                     print("Constraint Default not Found")
                     print(e)      
 
@@ -110,11 +116,7 @@ if main_table_name in cloned_tables:
             Base.metadata.create_all(cloned_engine)    
 else:
     print(f"Table {main_table_name} Not Found in {cloned_db}.{cloned_schema}")
-    exit()
-
-
-
-
+    exit()  
 
 
 
